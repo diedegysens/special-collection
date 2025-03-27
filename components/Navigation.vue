0e3bdbd84926167e4a87e9d8a9561f793cca1199
diedegysens <162812428+diedegysens@users.noreply.github.com>
2025-03-27 09:09:35 +0100

    commit

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,10 +2,12 @@
     <div>
         <nav class="mb-2">
         <ul>
-            <li class="pr-2"><NuxtLink to="/about">about us</NuxtLink></li>
+            <li class="pr-2"><NuxtLink to="/contact us">contact us</NuxtLink></li>
             <li class="pr-2"><NuxtLink to="/books">books</NuxtLink></li>
             <li class="pr-2"><NuxtLink to="/individuelbook">individuel book</NuxtLink></li>
             <li class="pr-2"><NuxtLink to="/signin">sign in</NuxtLink></li>
+            <li class="pr-2"><NuxtLink to="/discover our collection">discover</NuxtLink></li>
+
         </ul>
         </nav>
     </div>
